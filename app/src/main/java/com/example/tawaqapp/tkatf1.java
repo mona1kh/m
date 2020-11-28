0a55554f9f6d542a3050494bbd400ed4f5276539
lama11211 <llama11211@gmail.com>
2020-11-28 18:20:04 +0300

    h

---
+++ app/src/main/java/com/example/tawaqapp/tkatf1.java
@@ -0,0 +1,14 @@
+package com.example.tawaqapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tkatf1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tkatf1);
+    }
+}
